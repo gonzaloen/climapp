2fcbdd3dc1db80bf44b6c81c1ff0a3922abb0120
gonzaloen <34185901+gonzaloen@users.noreply.github.com>
2024-11-05 18:32:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-import { apiKey } from './config.js';
-
-async function getWeather() {
+// app.js
+export function getWeather() {
     const city = document.getElementById("city").value;
-    console.log("API Key:", apiKey); // Mostrar la clave en la consola para verificar que esté presente
-
+    const apiKey = "9c54ae518f01b4a75caf7490c67ff151";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=es`;
 
     try {
         const response = await fetch(url);
-        console.log("Response status:", response.status); // Log para ver el estado de la respuesta
+        console.log("Response status:", response.status);
 
         if (!response.ok) throw new Error("Ciudad no encontrada");
 
         const data = await response.json();
-        console.log("Data recibida:", data); // Verificar si los datos son correctos
+        console.log("Data recibida:", data);
 
         const weatherInfo = `
             <h2>Clima en ${data.name}</h2>
@@ -24,7 +22,7 @@ async function getWeather() {
 
         document.getElementById("weather-result").innerHTML = weatherInfo;
     } catch (error) {
-        console.error("Error:", error.message); // Log de error
+        console.error("Error:", error.message);
         document.getElementById("weather-result").innerHTML = `<p>${error.message}</p>`;
     }
 }
